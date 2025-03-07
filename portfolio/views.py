b4b1c8dd71a5501b4390087c795a27747fd6286d
VictorrMendes <victo.mendes.souza@gmail.com>
2025-03-06 21:04:58 -0300

    ajustando varios projetos

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,19 +4,22 @@ from django.conf import settings
 
 class ProjectsViews(views.APIView):
     def get(self, request):
-        project = Projects.objects.last()
+        projects = Projects.objects.all()
 
-        if project is None:
+        if not projects.exists():
             return response.Response({"message": "Nenhum projeto encontrado"}, status=404)
 
-        
-        image_url = request.build_absolute_uri(project.projetc_img.url) 
+        projects_data = []
+        for project in projects:
+            image_url = request.build_absolute_uri(project.projetc_img.url)
 
-        return response.Response({
-            "title": project.title,
-            "projetc_img": image_url,  
-            "tools": project.tools,
-            "title_back": project.title_back,
-            "description": project.description,
-            "link_project": project.link_project
-        })
+            projects_data.append({
+                "title": project.title,
+                "projetc_img": image_url,  
+                "tools": project.tools,
+                "title_back": project.title_back,
+                "description": project.description,
+                "link_project": project.link_project
+            })
+
+        return response.Response(projects_data)
